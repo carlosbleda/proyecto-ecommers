0efdb5e22294182c00718634b646ce3d20d200d2
carlosbleda <carlosbleda3@gmail.com>
2021-08-22 19:42:16 -0300

    boton de google2

--- js/login.js
+++ js/login.js
@@ -52,8 +52,8 @@ function onSignIn(googleUser) {
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
-window.location = "index.html" //redireccion a la pagina principal
+
+    window.location = "index.html" //redireccion a la pagina principal
 }
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
